62c3cfd70dc691e45abf25365afef4c7bc5c1db3
Jorge Gongora <jorge.gongora2610@gmail.com>
2021-08-16 00:56:52 -0500

    login generated

--- auth_api/python/api.py
+++ auth_api/python/api.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from methods import Token, Restricted
+from flask import abort
+from methods import login, Restricted
+import hashlib
+import jwt
 
 app = Flask(__name__)
-login = Token()
+login = login()
 protected = Restricted()
 
 
@@ -25,8 +28,23 @@ def url_health():
 def url_login():
     username = request.form['username']
     password = request.form['password']
+    seed = "my2w7wjd7yXF64FIADfJxNs1oupTGAuW"
+    if username == "admin":
+      salt = "F^S%QljSfV"
+      role = "admin"
+    if username == "noadmin":
+        salt = "KjvFUC#K*i"
+        role = "editor"
+    if username == "bob":
+        salt = "F^S%QljSfV"
+        role = "viewer"
+    salted_password = password + salt
+    hashed_PWD = hashlib.sha512( str( salted_password ).encode("utf-8") ).hexdigest()
+    encoded_jwt = jwt.encode({role: role}, seed, algorithm="HS256")
+
+    print(encoded_jwt)
     res = {
-        "data": login.generate_token(username, password)
+        "data": encoded_jwt
     }
     return jsonify(res)
 
@@ -35,8 +53,16 @@ def url_login():
 @app.route("/protected")
 def url_protected():
     auth_token = request.headers.get('Authorization')
+    word_list = auth_token.split()
+    token = word_list[1]
+    seed = "my2w7wjd7yXF64FIADfJxNs1oupTGAuW"
+    payload =jwt.decode(token, seed, algorithms=["HS256"])
+
+    if payload.get('role') != 'admin':
+        abort(401, description="You don't have access")
+
     res = {
-        "data": protected.access_data(auth_token)
+        "data": "You are under protected data"
     }
     return jsonify(res)
 
--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,8 +1,16 @@
 # These functions need to be implemented
-class Token:
+from flask.globals import request
+from flask.json import jsonify
 
+class login:
+
+    # Generar el token
     def generate_token(self, username, password):
-        return 'test'
+        token = request.args.post(username,password)
+        print(token)
+        if not token:
+            return jsonify({'message': 'missing token'}), 403
+        return token
 
 
 class Restricted:
